88c5d81165c55cba9ddac65ca0aac203b3d96820
SusiManjon <smanjonblanco@gmail.com>
2026-06-16 05:40:50 -0400

    Corregir espacio del ícono en buscadores

--- resources/js/Pages/admin/ProductsList.vue
+++ resources/js/Pages/admin/ProductsList.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6 gap-4">
       <div class="relative w-full sm:w-96">
-        <input type="text" v-model="search" @input="debouncedFetch" placeholder="Buscar productos..." class="form-input pl-10 w-full">
+        <input type="text" v-model="search" @input="debouncedFetch" placeholder="Buscar productos..." class="form-input !pl-10 w-full">
         <svg class="w-5 h-5 text-gray-400 absolute left-3 top-1/2 -translate-y-1/2" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
       </div>
       <router-link :to="{ name: 'admin.products.create' }" class="btn-primary shrink-0">
